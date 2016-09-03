33eb6497bc99f58a28209f2437bfb2b8d83a04ed
VivaNova99 <veraivanova9999@gmail.com>
2016-09-03 15:59:35 +0300

    Module 1 task 4

---
+++ Homework01-4/src/main/java/DimensionalArray.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * Created by veraivanova on 03.09.16.
+ */
+public class DimensionalArray {
+    public static void main(String[] args) {
+
+        System.out.println("Введите 'n' - длину последовательности");
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] a = new int[2*n];
+        int max = 0;
+
+        for (int i=1; i <= 2*n; i++) {
+
+            a[i-1] = i;
+
+//            System.out.println("a["+(i)+"] = "+a[i]);
+
+        }
+
+        for (int i = 1; i <= n; i++) {
+
+            int element = a[i-1] + a[2*n-i];
+            if (max < element)
+                max = element;
+
+        }
+
+        System.out.println("наибольший элемент в последовательности = " + max);
+    }
+}
